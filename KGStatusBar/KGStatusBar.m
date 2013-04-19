016d7e4dd2a52e20ffe99fc0c99aa1bf386ec3f7
Danylo Kostyshyn <danylo.kostyshyn@gmail.com>
2013-04-19 12:35:42 +0300

    Added orientation changes support.

--- KGStatusBar/KGStatusBar.m
+++ KGStatusBar/KGStatusBar.m
@@ -103,13 +103,24 @@
         overlayWindow.backgroundColor = [UIColor clearColor];
         overlayWindow.userInteractionEnabled = NO;
         overlayWindow.windowLevel = UIWindowLevelStatusBar;
+        
+        // Transform depending on interafce orientation
+        CGAffineTransform rotationTransform = CGAffineTransformMakeRotation([self rotation]);
+        self.overlayWindow.transform = rotationTransform;
+        self.overlayWindow.bounds = CGRectMake(0.f, 0.f, [self rotatedSize].width, [self rotatedSize].height);
+        
+        // Register for orientation changes
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(handleRoration:)
+                                                     name:UIApplicationDidChangeStatusBarOrientationNotification
+                                                   object:nil];
     }
     return overlayWindow;
 }
 
 - (UIView *)topBar {
     if(!topBar) {
-        topBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, overlayWindow.frame.size.width, 20.0)];
+        topBar = [[UIView alloc] initWithFrame:CGRectMake(0.f, 0.f, [self rotatedSize].width, 20.0f)];
         [overlayWindow addSubview:topBar];
     }
     return topBar;
@@ -131,12 +142,58 @@
 		stringLabel.shadowColor = [UIColor blackColor];
 		stringLabel.shadowOffset = CGSizeMake(0, -1);
         stringLabel.numberOfLines = 0;
+        stringLabel.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
     }
     
     if(!stringLabel.superview)
         [self.topBar addSubview:stringLabel];
     
     return stringLabel;
+}
+
+#pragma mark - Handle Rotation
+
+- (CGFloat)rotation
+{
+    UIInterfaceOrientation interfaceOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    CGFloat rotation = 0.f;
+    switch (interfaceOrientation) {
+        case UIInterfaceOrientationLandscapeLeft: { rotation = -M_PI_2; } break;
+        case UIInterfaceOrientationLandscapeRight: { rotation = M_PI_2; } break;
+        case UIInterfaceOrientationPortraitUpsideDown: { rotation = M_PI; } break;
+        case UIInterfaceOrientationPortrait: { } break;
+        default: break;
+    }
+    return rotation;
+}
+
+- (CGSize)rotatedSize
+{
+    UIInterfaceOrientation interfaceOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+    CGSize rotatedSize = screenSize;
+    switch (interfaceOrientation) {
+        case UIInterfaceOrientationLandscapeLeft: { rotatedSize = CGSizeMake(screenSize.height, screenSize.width); } break;
+        case UIInterfaceOrientationLandscapeRight: { rotatedSize = CGSizeMake(screenSize.height, screenSize.width); } break;
+        case UIInterfaceOrientationPortraitUpsideDown: { } break;
+        case UIInterfaceOrientationPortrait: { } break;
+        default: break;
+    }
+    return rotatedSize;
+}
+
+- (void)handleRoration:(id)sender
+{
+    // Based on http://stackoverflow.com/questions/8774495/view-on-top-of-everything-uiwindow-subview-vs-uiviewcontroller-subview
+    
+    CGAffineTransform rotationTransform = CGAffineTransformMakeRotation([self rotation]);
+    [UIView animateWithDuration:[[UIApplication sharedApplication] statusBarOrientationAnimationDuration]
+                       animations:^{
+                           self.overlayWindow.transform = rotationTransform;
+                           // Transform invalidates the frame, so use bounds/center
+                           self.overlayWindow.bounds = CGRectMake(0.f, 0.f, [self rotatedSize].width, [self rotatedSize].height);
+                           self.topBar.frame = CGRectMake(0.f, 0.f, [self rotatedSize].width, 20.f);
+                       }];
 }
 
 @end
