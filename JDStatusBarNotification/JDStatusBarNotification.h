be656ad84fc41db626213fc20c803e8bf06ab389
Markus Emrich <Markus.Emrich@gmail.com>
2013-10-31 12:13:53 +0100

    starting header documentation

--- JDStatusBarNotification/JDStatusBarNotification.h
+++ JDStatusBarNotification/JDStatusBarNotification.h
@@ -9,21 +9,41 @@
 
 #import <UIKit/UIKit.h>
 
-extern NSString *const JDStatusBarStyleError;
-extern NSString *const JDStatusBarStyleWarning;
-extern NSString *const JDStatusBarStyleSuccess;
+extern NSString *const JDStatusBarStyleError;   /// This style has a red background with a white Helvetica label.
+extern NSString *const JDStatusBarStyleWarning; /// This style has a yellow background with a gray Helvetica label.
+extern NSString *const JDStatusBarStyleSuccess; /// This style has a green background with a white Helvetica label.
 
-typedef enum {
-    JDStatusBarAnimationTypeMove,
-    JDStatusBarAnimationTypeFade
-} JDStatusBarAnimationType;
+typedef NS_ENUM(NSInteger, JDStatusBarAnimationType) {
+    JDStatusBarAnimationTypeMove, /// Notification will move in from the top, and hide again to the top
+    JDStatusBarAnimationTypeFade  /// Notification will fade in and out
+};
 
 @class JDStatusBarStyle;
 typedef JDStatusBarStyle*(^JDPrepareStyleBlock)(JDStatusBarStyle *style);
 
+/**
+ *  This class is a singletion which is used to present notifications 
+ *  on top of the status bar. To present a notification, use one of the
+ *  given class methods.
+ */
 @interface JDStatusBarNotification : UIView
 
+/**
+ *  Show a notification. It won't hide automatically, 
+ *  you have to dimiss it on your own.
+ *
+ *  @param status The message to display
+ */
 + (void)showWithStatus:(NSString *)status;
+
+/**
+ *  Show a notification with a specific style. It won't 
+ *  hide automatically, you have to dimiss it on your own.
+ *
+ *  @param status The message to display
+ *  @param styleName The name of the style. You can use any JDStatusBarStyle, 
+ *  or a custom style, after you added one. If this is nil, the default style will be used.
+ */
 + (void)showWithStatus:(NSString *)status
              styleName:(NSString*)styleName;
 
