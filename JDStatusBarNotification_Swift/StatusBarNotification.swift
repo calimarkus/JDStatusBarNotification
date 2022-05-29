8da241459f58800a4ce4c246317d9ac02c793fc2
Markus Emrich <Markus.Emrich@gmail.com>
2022-05-29 16:30:26 +0900

    make swift compile again, convert to swift 5, make it callable from objc

--- JDStatusBarNotification_Swift/StatusBarNotification.swift
+++ JDStatusBarNotification_Swift/StatusBarNotification.swift
@@ -9,212 +9,193 @@
 import Foundation
 import UIKit
 
-class StatusBarNotification {
-    
-    var dismissTimer:NSTimer?
-    var progress:CGFloat = 0
-    
-    var activeStyle:StatusBarStyle
-    var defaultStyle:StatusBarStyle
-    var userStyles:Dictionary<String,StatusBarStyle>
-    
-    var overlayWindow:UIWindow?
-    var progressView:UIView?
-    var topBar:StatusBarView?
-    
+@objc
+public class StatusBarNotification : NSObject
+{
+    var dismissTimer: Timer? = nil
+    var progress: Double = 0.0
+
+    var activeStyle: StatusBarStyle
+    var defaultStyle: StatusBarStyle
+    var userStyles: [String: StatusBarStyle] = [:]
+
+    var overlayWindow: UIWindow? = nil
+    var progressView: UIView? = nil
+    var topBar: StatusBarView? = nil
+
     // ---------------------------------- Singleton ------------------------------------------ //
-    
+
     class func sharedInstance() -> StatusBarNotification
     {
-        struct Shared {
+        enum Shared
+        {
             static let instance = StatusBarNotification()
         }
         return Shared.instance
     }
-    
+
     // -------------------------------- Class methods ---------------------------------------- //
 
     // Presentation
-    class func showWithStatus(status:String) -> StatusBarView
+    @objc
+    public class func showWithStatus(_ status: String) -> StatusBarView
     {
-        return showWithStatus(status, dismissAfter:nil, styleName:nil)
-    }
-    
-    class func showWithStatus(status:String, styleName:String?) -> StatusBarView
-    {
-        return showWithStatus(status, dismissAfter:nil, styleName:styleName)
-    }
-    
-    class func showWithStatus(status:String, dismissAfter:NSTimeInterval?) -> StatusBarView
-    {
-        return showWithStatus(status, dismissAfter:dismissAfter, styleName:nil)
-    }
-    
-    class func showWithStatus(status:String, dismissAfter:NSTimeInterval?, styleName:String?) -> StatusBarView
-    {
-        return StatusBarView(frame: CGRectMake(0, 0, 320, 40))
-    }
-    
-    // Dismissal
-    class func dismiss()
-    {
-        dismissAnimated(true)
-    }
-    
-    class func dismissAnimated(animated:Bool)
-    {
-        sharedInstance().dismissAnimated(true)
-    }
-    
-    class func dismissAfter(delay:NSTimeInterval)
-    {
-        sharedInstance().setDismissTimer(interval:delay)
-    }
-    
-    // Styles
-    class func setDefaultStyle(prepare: (style:StatusBarStyle) -> StatusBarStyle)
-    {
-        
-    }
-    
-    class func addStyleNamed(identifier:String, prepare: (style:StatusBarStyle) -> StatusBarStyle)
-    {
-        
-    }
-    
-    // progress & activity
-    class func showProgress(progress:Float)
-    {
-        
-    }
-    
-    class func showActivityIndicator(show:Bool, indicatorStyle:UIActivityIndicatorViewStyle)
-    {
-        
+        return showWithStatus(status, dismissAfter: nil, styleName: nil)
     }
 
+    public class func showWithStatus(_ status: String, dismissAfter: TimeInterval? = nil, styleName: String? = nil) -> StatusBarView
+    {
+        return StatusBarView(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+    }
+
+    // Dismissal
+    public class func dismiss()
+    {
+        dismissAnimated(animated: true)
+    }
+
+    public class func dismissAnimated(animated: Bool)
+    {
+        sharedInstance().dismissAnimated(animated: true)
+    }
+
+    public class func dismissAfter(delay: TimeInterval)
+    {
+        sharedInstance().setDismissTimer(interval: delay)
+    }
+
+    // Styles
+    public class func setDefaultStyle(prepare: (_ style: StatusBarStyle) -> StatusBarStyle) {}
+    public class func addStyleNamed(identifier: String, prepare: (_ style: StatusBarStyle) -> StatusBarStyle) {}
+
+    // progress & activity
+    public class func showProgress(progress: Float) {}
+    public class func showActivityIndicator(show: Bool, indicatorStyle: UIActivityIndicatorView.Style) {}
+
     // State
-    class func isVisible() -> Bool
+    public class func isVisible() -> Bool
     {
         return false
     }
-    
+
     // ---------------------------------- Implementation ------------------------------------ //
-    
-    init()
+
+    override init()
     {
         defaultStyle = StatusBarStyle.statusBarStyleForPreset(.Default)
         activeStyle = defaultStyle
-        
-        userStyles = Dictionary<String,StatusBarStyle>()
         for styleName in StatusBarStyle.allDefaultBarStyles() {
-            userStyles[styleName.toRaw()] = StatusBarStyle.statusBarStyleForPreset(styleName)
+            userStyles[styleName.rawValue] = StatusBarStyle.statusBarStyleForPreset(styleName)
         }
-        
+        super.init()
+
         // register for orientation changes
-        NSNotificationCenter.defaultCenter().addObserver(self, selector:"willChangeStatusBarFrame:",
-            name:UIApplicationWillChangeStatusBarFrameNotification, object:nil)
+        weak var weakself = self
+        NotificationCenter.default.addObserver(forName: UIApplication.willChangeStatusBarFrameNotification,
+                                               object: nil,
+                                               queue: nil)
+        { notif in
+            weakself?.willChangeStatusBarFrame(notification: notif)
+        }
     }
-    
+
     deinit
     {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+        NotificationCenter.default.removeObserver(self)
     }
-    
+
     // dismissing
-    func setDismissTimer(#interval:NSTimeInterval)
+    func setDismissTimer(interval: TimeInterval)
     {
-        let fireDate = NSDate(timeIntervalSinceNow:interval)
-        
+        let fireDate = Date(timeIntervalSinceNow: interval)
+
         dismissTimer?.invalidate()
-        dismissTimer = NSTimer(fireDate:fireDate, interval:0, target:self, selector:"dismiss:", userInfo:nil, repeats:false)
-        NSRunLoop.currentRunLoop().addTimer(dismissTimer, forMode: NSRunLoopCommonModes)
+        let newTimer = Timer(fire: fireDate, interval: 0, repeats: false, block: { _ in
+            self.dismissAnimated(animated: true)
+        })
+        RunLoop.current.add(newTimer, forMode: .common)
+        dismissTimer = newTimer
     }
-    
-    func dismiss(timer:NSTimer)
-    {
-        dismissAnimated(true)
-    }
-    
-    func dismissAnimated(animated:Bool)
+
+    func dismissAnimated(animated: Bool)
     {
         dismissTimer?.invalidate()
         dismissTimer = nil
-        
-        if (!self.overlayWindow) { return }
-        if (!self.topBar) { return }
-    
+
+        guard let overlayWindow = self.overlayWindow else { return }
+        guard let topBar = self.topBar else { return }
+
         // check animation type
         let animationsEnabled = (activeStyle.animationType != StatusBarStyle.AnimationType.None)
         let shouldAnimate = animated && animationsEnabled
 
         // animate out
-        let topBar:UIView = self.topBar!
-        let overlayWindow:UIWindow = self.overlayWindow!
-        UIView.animateWithDuration(shouldAnimate ? 0.4 : 0.0, animations: {
-            if (self.activeStyle.animationType == StatusBarStyle.AnimationType.Fade) {
+        UIView.animate(withDuration: shouldAnimate ? 0.4 : 0.0, animations: {
+            if self.activeStyle.animationType == StatusBarStyle.AnimationType.Fade
+            {
                 topBar.alpha = 0.0
-            } else {
-                topBar.transform = CGAffineTransformMakeTranslation(0, -topBar.frame.size.height)
             }
-            }, completion: {(completed) in
-                self.overlayWindow!.removeFromSuperview()
-                self.overlayWindow!.hidden = true
-                self.overlayWindow!.rootViewController = nil
-                self.overlayWindow = nil
-                self.progressView = nil
-                self.topBar = nil
-            })
+            else
+            {
+                topBar.transform = CGAffineTransform(translationX: 0, y: -topBar.frame.size.height)
+            }
+        }, completion: { _ in
+            overlayWindow.removeFromSuperview()
+            overlayWindow.isHidden = true
+            overlayWindow.rootViewController = nil
+            self.overlayWindow = nil
+            self.progressView = nil
+            self.topBar = nil
+        })
     }
-    
-    func addStyleNamed(identifier:String, prepare:(style:StatusBarStyle) -> StatusBarStyle) -> String
+
+    func addStyleNamed(identifier: String, prepare: (_ style: StatusBarStyle) -> StatusBarStyle) -> String
     {
-        userStyles[identifier] = prepare(style:defaultStyle)
-        return identifier;
+        userStyles[identifier] = prepare(defaultStyle)
+        return identifier
     }
-    
+
     // Rotation
     func updateWindowTransform()
     {
-        if (!overlayWindow) { return }
-        
-        var window = UIApplication.sharedApplication().keyWindow
-        if (window == nil && UIApplication.sharedApplication().windows.count > 0) {
-            window = UIApplication.sharedApplication().windows[0] as UIWindow
+        guard let overlayWindow = overlayWindow else { return }
+
+        let window = UIApplication.shared.keyWindow ?? UIApplication.shared.windows.first
+        if let w = window
+        {
+            overlayWindow.transform = w.transform
+            overlayWindow.frame = w.frame
         }
-        overlayWindow!.transform = window.transform
-        overlayWindow!.frame = window.frame
     }
-    
-    func updateTopBarFrameWithStatusBarFrame(rect:CGRect)
+
+    func updateTopBarFrameWithStatusBarFrame(rect: CGRect)
     {
-        if (!topBar) { return }
-        
-        let width:CGFloat  = max(rect.size.width, rect.size.height)
-        let height:CGFloat = min(rect.size.width, rect.size.height)
-        
+        guard let topBar = topBar else { return }
+
+        let width: CGFloat = max(rect.size.width, rect.size.height)
+        let height: CGFloat = min(rect.size.width, rect.size.height)
+
         // on ios7 fix position, if statusBar has double height
-        var yPos:CGFloat = 0;
-        if (UIDevice.currentDevice().systemVersion.bridgeToObjectiveC().floatValue >= 7.0 && height > 20.0) {
-            yPos = -height/2.0;
+        var yPos: CGFloat = 0
+        if Float(UIDevice.current.systemVersion) ?? 0.0 >= 7.0, height > 20.0
+        {
+            yPos = -height / 2.0
         }
-        
-        topBar!.frame = CGRectMake(0, yPos, width, height);
+
+        topBar.frame = CGRect(x: 0, y: yPos, width: width, height: height)
     }
-    
-    func willChangeStatusBarFrame(notification:NSNotification)
+
+    func willChangeStatusBarFrame(notification: Notification)
     {
-        let barFrameValue:NSValue = notification.userInfo[UIApplicationStatusBarFrameUserInfoKey] as NSValue
-        UIView.animateWithDuration(0.5) {
+        let barFrameValue: NSValue = notification.userInfo?[UIApplication.statusBarFrameUserInfoKey] as! NSValue
+        UIView.animate(withDuration: 0.5)
+        {
             self.updateWindowTransform()
-            self.updateTopBarFrameWithStatusBarFrame(barFrameValue.CGRectValue())
-            
+            self.updateTopBarFrameWithStatusBarFrame(rect: barFrameValue.cgRectValue)
+
             // update progress with current value
-            let progress = self.progress;
-            self.progress = progress;
+            let progress = self.progress
+            self.progress = progress
         }
     }
-
 }
-
-
--- JDStatusBarNotification_Swift/StatusBarView.swift
+++ JDStatusBarNotification_Swift/StatusBarView.swift
@@ -9,62 +9,65 @@
 import Foundation
 import UIKit
 
+@objc
+public class StatusBarView: UIView {
+    public var textLabel: UILabel
+    public var activityIndicatorView: UIActivityIndicatorView
 
-class StatusBarView : UIView {
-    
-    var textLabel :UILabel
-    var activityIndicatorView :UIActivityIndicatorView
-    
-    var textVerticalPositionAdjustment :CGFloat {
-    didSet{ self.setNeedsLayout() }
+    public var textVerticalPositionAdjustment: CGFloat {
+        didSet { self.setNeedsLayout() }
     }
-    
-    init(frame: CGRect) {
+
+    override init(frame: CGRect) {
         textLabel = UILabel()
-        activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .White)
-        textVerticalPositionAdjustment = 0.0;
+        activityIndicatorView = .init(style: .medium)
+        textVerticalPositionAdjustment = 0.0
         super.init(frame: frame)
         setupLabel()
         setupActivityIndicator()
     }
 
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     func setupLabel() {
-        textLabel.backgroundColor = UIColor.clearColor()
-        textLabel.baselineAdjustment = .AlignCenters
-        textLabel.textAlignment = .Center
+        textLabel.backgroundColor = .clear
+        textLabel.baselineAdjustment = .alignCenters
+        textLabel.textAlignment = .center
         textLabel.adjustsFontSizeToFitWidth = true
         textLabel.clipsToBounds = true
         addSubview(textLabel)
     }
-    
+
     func setupActivityIndicator() {
-        activityIndicatorView.transform = CGAffineTransformMakeScale(0.7, 0.7)
+        activityIndicatorView.transform = .init(scaleX: 0.7, y: 0.7)
         activityIndicatorView.hidesWhenStopped = true
         addSubview(activityIndicatorView)
     }
-    
-    override func layoutSubviews() {
-        
+
+    public override func layoutSubviews() {
         // label
         var labelRect = bounds
         labelRect.origin.x = 0
         labelRect.origin.y = 1+textVerticalPositionAdjustment
         labelRect.size.height -= 1
-        self.textLabel.frame = labelRect
-        
+        textLabel.frame = labelRect
+
         // activity indicator
-        if !activityIndicatorView.hidden {
+        if !activityIndicatorView.isHidden {
             let textSize = currentTextSize()
-            let availableSpaceHalf = ((self.bounds.size.width - textSize.width)/2.0)
+            let availableSpaceHalf = ((bounds.size.width - textSize.width)/2.0)
             var indicatorFrame = activityIndicatorView.frame
             indicatorFrame.origin.x = availableSpaceHalf - indicatorFrame.size.width - 8.0
-            indicatorFrame.origin.y = (1+(self.bounds.size.height - indicatorFrame.size.height)/2.0)
-            activityIndicatorView.frame = CGRectIntegral(indicatorFrame)
+            indicatorFrame.origin.y = (1+(bounds.size.height - indicatorFrame.size.height)/2.0)
+            activityIndicatorView.frame = indicatorFrame.integral
         }
     }
-    
+
     func currentTextSize() -> CGSize {
-        var attributes = [NSFontAttributeName:textLabel.font]
-        return textLabel.text.bridgeToObjectiveC().sizeWithAttributes(attributes)
+        let attributes: [NSAttributedString.Key: Any] = [.font: textLabel.font!]
+        return textLabel.text?.size(withAttributes: attributes) ?? .zero
     }
 }
