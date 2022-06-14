2043cc0a7bbcd537353052404264b7b67954ac2d
Markus Emrich <Markus.Emrich@gmail.com>
2022-06-14 12:24:53 +0900

    Support SPM

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.6
+// (minimum version of Swift required to build this package)
+
+import PackageDescription
+
+let package = Package(
+    name: "JDStatusBarNotification",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "JDStatusBarNotification",
+            targets: ["JDStatusBarNotification"]),
+    ],
+    targets: [
+        .target(
+            name: "JDStatusBarNotification",
+            dependencies: [],
+            path: ".",
+            exclude: ["ExampleProject/"],
+            sources: ["JDStatusBarNotification/"],
+            publicHeadersPath: "JDStatusBarNotification/Public/",
+            cSettings: [
+                .headerSearchPath("**"),
+            ]),
+    ]
+)
