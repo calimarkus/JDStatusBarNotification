3bb75bff4f4ad5ee9944cc13def4ac265330134b
Markus Emrich <Markus.Emrich@gmail.com>
2023-11-05 11:45:14 +0100

    move internal JDSBNotificationView.h back to non-library-style imports

--- JDStatusBarNotification/Private/JDSBNotificationView.h
+++ JDStatusBarNotification/Private/JDSBNotificationView.h
@@ -7,7 +7,7 @@
 
 #import <UIKit/UIKit.h>
 
-#import <JDStatusBarNotification/JDStatusBarNotificationPresenterCustomViewSizingController.h>
+#import "JDStatusBarNotificationPresenterCustomViewSizingController.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
