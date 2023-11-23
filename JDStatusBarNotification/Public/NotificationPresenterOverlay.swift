d967ab6217cc4dd1cc9a5177f4afefe3ce83356e
Markus Emrich <Markus.Emrich@gmail.com>
2023-11-24 00:56:49 +0100

    add missing presentation variants for objc compatibility

--- JDStatusBarNotification/Public/NotificationPresenterOverlay.swift
+++ JDStatusBarNotification/Public/NotificationPresenterOverlay.swift
@@ -10,13 +10,231 @@ import UIKit
 
 public extension NotificationPresenter {
 
+
+  // MARK: - Presentation (default style)
+
+  /// Present a notification
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:)
+  @available(*, unavailable)
+  func present(_ text: String) -> UIView
+  {
+    present(text, subtitle: nil, styleName: nil, duration: 0.0, completion: nil)
+  }
+
+  /// Present a notification
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - completion: A ``Completion`` closure, which gets called once the presentation animation finishes. It won't be called after dismissal.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:completion:)
+  @available(*, unavailable)
+  func present(_ text: String, completion: Completion?) -> UIView
+  {
+    present(text, subtitle: nil, styleName: nil, duration: 0.0, completion: completion)
+  }
+
+  /// Present a notification
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - completion: A ``Completion`` closure, which gets called once the presentation animation finishes. It won't be called after dismissal.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithTitle:subtitle:completion:)
+  @available(*, unavailable)
+  func present(_ title: String, subtitle: String, completion: Completion?) -> UIView
+  {
+    present(title, subtitle: subtitle, styleName: nil, duration: 0.0, completion: completion)
+  }
+
+  /// Present a notification
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:dismissAfterDelay:)
+  @available(*, unavailable)
+  func present(_ text: String, delay: CGFloat) -> UIView
+  {
+    present(text, subtitle: nil, styleName: nil, duration: delay, completion: nil)
+  }
+
+
+  // MARK: - Presentation (custom style)
+
+  /// Present a notification using the default style or a named style.
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - customStyle: The name of the style. You can use styles previously added using e.g. ``addStyle(named:usingStyle:prepare:)``.
+  ///                  If no style can be found for the given `styleName` or it is `nil`, the default style will be used.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:customStyle:)
+  @available(*, unavailable)
+  func present(_ text: String, customStyle: String) -> UIView
+  {
+    present(text, subtitle: nil, styleName: customStyle, duration: 0.0, completion: nil)
+  }
+
+  /// Present a notification using the default style or a named style.
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - customStyle: The name of the style. You can use styles previously added using e.g. ``addStyle(named:usingStyle:prepare:)``.
+  ///                  If no style can be found for the given `styleName` or it is `nil`, the default style will be used.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:customStyle:completion:)
+  @available(*, unavailable)
+  func present(_ text: String, customStyle: String?, completion: Completion?) -> UIView
+  {
+    present(text, subtitle: nil, styleName: customStyle, duration: 0.0, completion: completion)
+  }
+
+  /// Present a notification using the default style or a named style.
+  ///
+  /// - Parameters:
+  ///   - title: The text to display as title
+  ///   - subtitle: The text to display as subtitle
+  ///   - styleName: The name of the style. You can use styles previously added using e.g. ``addStyle(named:usingStyle:prepare:)``.
+  ///                If no style can be found for the given `styleName` or it is `nil`, the default style will be used.
+  ///   - completion: A ``Completion`` closure, which gets called once the presentation animation finishes. It won't be called after dismissal.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithTitle:subtitle:customStyle:completion:)
+  @available(*, unavailable)
+  func present(_ title: String,
+               subtitle: String?,
+               customStyle: String?,
+               completion: Completion?) -> UIView
+  {
+    present(title, subtitle: subtitle, styleName: customStyle, duration: 0.0, completion: completion)
+  }
+
+  /// Present a notification using the default style or a named style.
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - customStyle: The name of the style. You can use styles previously added using e.g. ``addStyle(named:usingStyle:prepare:)``.
+  ///                  If no style can be found for the given `styleName` or it is `nil`, the default style will be used.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:dismissAfterDelay:customStyle:)
+  @available(*, unavailable)
+  func present(_ text: String, delay: CGFloat, customStyle: String) -> UIView
+  {
+    present(text, subtitle: nil, styleName: customStyle, duration: delay, completion: nil)
+  }
+
+  // MARK: - Presentation (included style)
+
+  /// Present a notification using an included style.
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - includedStyle: An existing ``IncludedStatusBarNotificationStyle``
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:includedStyle:)
+  @available(*, unavailable)
+  func present(_ text: String, includedStyle: IncludedStatusBarNotificationStyle) -> UIView
+  {
+    present(text, subtitle: nil, includedStyle: includedStyle, duration: 0.0, completion: nil)
+  }
+
+  /// Present a notification using an included style.
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - includedStyle: An existing ``IncludedStatusBarNotificationStyle``
+  ///   - completion: A ``Completion`` closure, which gets called once the presentation animation finishes. It won't be called after dismissal.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:includedStyle:completion:)
+  @available(*, unavailable)
+  func present(_ text: String,
+               includedStyle: IncludedStatusBarNotificationStyle,
+               completion: Completion?) -> UIView
+  {
+    present(text, subtitle: nil, includedStyle: includedStyle, duration: 0.0, completion: completion)
+  }
+
+  /// Present a notification using an included style.
+  ///
+  /// - Parameters:
+  ///   - title: The text to display as title
+  ///   - subtitle: The text to display as subtitle
+  ///   - includedStyle: An existing ``IncludedStatusBarNotificationStyle``
+  ///   - completion: A ``Completion`` closure, which gets called once the presentation animation finishes. It won't be called after dismissal.
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithTitle:subtitle:includedStyle:completion:)
+  @available(*, unavailable)
+  func present(_ title: String,
+               subtitle: String?,
+               includedStyle: IncludedStatusBarNotificationStyle,
+               completion: Completion?) -> UIView
+  {
+    present(title, subtitle: subtitle, includedStyle: includedStyle, duration: 0.0, completion: completion)
+  }
+
+  /// Present a notification using an included style.
+  ///
+  /// - Parameters:
+  ///   - text: The text to display as title
+  ///   - duration: The duration defines how long the notification will be visible. If not provided the notifcation will never be dismissed.
+  ///   - includedStyle: An existing ``IncludedStatusBarNotificationStyle``
+  ///
+  /// - Returns: The presented UIView for further customization
+  ///
+  @discardableResult
+  @objc(presentWithText:dismissAfterDelay:includedStyle:)
+  @available(*, unavailable)
+  func present(_ text: String,
+               delay: CGFloat = 0.0,
+               includedStyle: IncludedStatusBarNotificationStyle) -> UIView
+  {
+    present(text, subtitle: nil, includedStyle: includedStyle, duration: delay, completion: nil)
+  }
+
   // MARK: - Dismissal
 
   /// Dismisses any currently displayed notification animated - after the provided delay, if provided.
   @objc
   @available(*, unavailable)
   func dismiss() {
-    dismiss(animated: true)
+    dismiss(animated: true, after: 0.0, completion: nil)
   }
 
   /// Dismisses any currently displayed notification animated - after the provided delay, if provided.
@@ -26,7 +244,7 @@ public extension NotificationPresenter {
   @objc(dismissWithCompletion:)
   @available(*, unavailable)
   func dismiss(completion: Completion?) {
-    dismiss(animated: true, completion: completion)
+    dismiss(animated: true, after: 0.0, completion: completion)
   }
 
   /// Dismisses any currently displayed notification animated - after the provided delay, if provided.
@@ -37,7 +255,7 @@ public extension NotificationPresenter {
   @objc(dismissAnimated:)
   @available(*, unavailable)
   func dismiss(animated: Bool) {
-    dismiss(animated: animated)
+    dismiss(animated: animated, after: 0.0, completion: nil)
   }
 
   /// Dismisses any currently displayed notification animated - after the provided delay, if provided.
@@ -47,7 +265,7 @@ public extension NotificationPresenter {
   @objc(dismissAfterDelay:)
   @available(*, unavailable)
   func dismiss(after delay: CGFloat) {
-    dismiss(animated: true, after: delay)
+    dismiss(animated: true, after: delay, completion: nil)
   }
 
   /// Dismisses any currently displayed notification animated - after the provided delay, if provided.
