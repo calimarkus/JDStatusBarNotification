754038f0cdb55dac2a5eea732260e75341c76ddb
Markus Emrich <Markus.Emrich@gmail.com>
2024-12-08 19:42:50 +0100

    add pods test project

---
+++ pods_testProject/pod_test/pod_testApp.swift
@@ -0,0 +1,17 @@
+//
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+@main
+struct pod_testApp: App {
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+          .navigationTitle("Pods Test App")
+      }
+    }
+  }
+}
